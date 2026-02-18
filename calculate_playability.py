f5c7b5454d19a6e2c2ee3f03ba9822186b97665c
archi221 <98173934+archi221@users.noreply.github.com>
2026-02-18 13:47:14 +0100

    kleine erweiterung für Karten einlesen

--- calculate_playability.py
+++ calculate_playability.py
@@ -12,26 +12,37 @@ def read_decklist(file):
     reading_step = Color.SPELLS
 
     deck = []
-    id_to_cards = {}
+    id_to_cards = []
     card_groups = {}
     id = 0
     with open(file) as f:
         for line in f:
 
             if line.strip(" \n") in ignored_names:
-                line_list = line.split(" ")
+                continue
+            if line.strip() == "\n":
+                continue
+
+            if reading_step == Color.SPELLS:
+                line_list = line.split(" ", maxsplit=1)
                 try:
                     amount = int(line_list[0])
-                    card_name_and_group = line_list[1]
+                    card_name_and_group = line_list[1].split(";")
                 except ValueError:
                     amount = 0
-                    card_name_and_group = line
+                    card_name_and_group = line.split(";")
                 except IndexError as e:
                     print(f"Error while reading amount: {e}")
                     continue
-                
-            if reading_step == Color.SPELLS:
-                continue
+                card_name = card_name_and_group[0]
+                group_list = card_name_and_group[1:]
+                id_to_cards.insert(id, card_name)
+                assert id_to_cards[id] == card_name, f"Karte: {card_name} wurde nicht korrekt an ID: {id} eingefügt"
+                id += 1
+                for i in range(amount):
+                    deck.append(card_name)
+                for group in group_list:
+                    card_groups.setdefault(group, []).append(card_name)
             elif reading_step == Color.PLAYABAL_HANDS:
                 continue
             elif reading_step == Color.GAMBA_EFFECTS:
